8431a6278b104e9a70c01eefb8c8ea7a9ea8e6d6
sushmitha-pothuganti <sushmithapothuganti2004@gmail.com>
2025-07-06 12:22:32 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB error:", err));
 
-// Start Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
