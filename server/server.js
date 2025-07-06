48cd8c4ab9a47bc2ed0f4fa256d196d53f2861a1
sushmitha-pothuganti <sushmithapothuganti2004@gmail.com>
2025-07-06 12:21:56 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/mood', require('./routes/mood'));
 app.use('/api/ai', require('./routes/ai'));
